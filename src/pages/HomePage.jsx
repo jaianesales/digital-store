ab4bc31ba96f5b9f72c467972dc7ab8e53b730e8
jaianesales <jaianekristhine@gmail.com>
2024-10-20 17:34:16 -0300

    product card component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,10 @@ import calcas from "../assets/calcas.svg";
 import camisetas from "../assets/camisetas.svg";
 import headphones from "../assets/headphones.svg";
 import tenis from "../assets/tenis.svg";
-import airJordan from "../assets/airJordan.png"
+import airJordan from "../assets/airJordan.png";
+import ProductCard from "../components/ProductCard";
+
+import tenisv8 from "../../public/tenisv8.png"
 
 const StyledImgColecao = styled.div`
   display: grid;
@@ -22,6 +25,22 @@ const StyledImgColecao = styled.div`
     img {
       width: 100%;
     }
+  }
+`;
+
+const StyledButtonPrimary = styled.button`
+  border: none;
+  border-radius: 4px;
+  padding: 16px 20px;
+  background-color: var(--primary);
+  font-weight: bold;
+  font-size: 1rem;
+  color: var(--white);
+  cursor: pointer;
+  transition: all ease-in-out 0.2s;
+
+  &:hover {
+    background-color: var(--tertiary);
   }
 `;
 
@@ -45,22 +64,6 @@ const StyledButtonSecondary = styled.button`
   }
 `;
 
-const StyledButtonPrimary = styled.button`
-  border: none;
-  border-radius: 4px;
-  padding: 16px 20px;
-  background-color: var(--primary);
-  font-weight: bold;
-  font-size: 1rem;
-  color: var(--white);
-  cursor: pointer;
-  transition: all ease-in-out 0.2s;
-
-  &:hover {
-    background-color: var(--tertiary);
-  }
-`;
-
 const StyledIconsContainer = styled.div`
   max-width: 800px;
   margin: 0 auto;
@@ -74,7 +77,7 @@ const StyledIcons = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 20px;
-  
+
   .icons-circle {
     max-width: 150px;
     border-radius: 50%;
@@ -98,137 +101,148 @@ const StyledIcons = styled.div`
   }
 `;
 
-const StyledOferta = styled.div `
-    display: grid;
-    grid-template-columns: 5fr 7fr;
-    gap: 40px;
+const StyledOferta = styled.div`
+  display: grid;
+  grid-template-columns: 5fr 7fr;
+  gap: 40px;
 
-    .airJordan {
-        padding: 20px;
+  .airJordan {
+    padding: 20px;
 
-        & img {
-            width: 100%;
-        }
+    & img {
+      width: 100%;
+      height: auto;
+      display: block;
+      object-fit: contain;
+    }
+  }
+
+  .informacao-oferta {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    gap: 20px;
+
+    & h4 {
+      color: var(--primary);
     }
 
-    .informacao-oferta {
-        display: flex;
-        flex-direction: column;
-        justify-content: start;
-        gap: 20px;
-
-        &h4 {
-            color: var(--primary);
-        }
-
-        .titulo-oferta {
-            color: var(--dark-grey-2);
-            font-size: 3rem;
-            font-weight: bold;
-        }
-
-        & button {
-            width: 200px;
-        }
+    .titulo-oferta {
+      color: var(--dark-grey-2);
+      font-size: 3rem;
+      font-weight: bold;
     }
+
+    & button {
+      width: 200px;
+    }
+  }
 `;
 
-
 function HomePage() {
-    return (
-        <>
-            <Section title={"Coleções em destaque"} color={"#F5F5F5"}>
-                <StyledImgColecao>
-                    <div>
-                        <img src={collection1} alt="" />
-                        <Link to={"/productlisting"}>
-                            <StyledButtonSecondary>Comprar</StyledButtonSecondary>
-                        </Link>
-                    </div>
-                    <div>
-                        <img src={collection2} alt="" />
-                        <Link to={"/productlisting"}>
-                            <StyledButtonSecondary>Comprar</StyledButtonSecondary>
-                        </Link>
-                    </div>
-                    <div>
-                        <img src={collection3} alt="" />
-                        <Link to={"/productlisting"}>
-                            <StyledButtonSecondary>Comprar</StyledButtonSecondary>
-                        </Link>
-                    </div>
-                </StyledImgColecao>
-            </Section>
-            <Section title={"Coleções em destaque"} titleAlign="center" color={"#F5F5F5"}>
-                <StyledIconsContainer>
-                    <StyledIcons>
-                        <Link to={"/productlisting"}>
-                            <div className="icons-circle">
-                                <img src={camisetas} alt="camisetas" />
-                            </div>
-                        </Link>
-                        <h4>Camisetas</h4>
-                    </StyledIcons>
-                    <StyledIcons>
-                        <Link to={"/productlisting"}>
-                            <div className="icons-circle">
-                                <img src={calcas} alt="calças" />
-                            </div>
-                        </Link>
-                        <h4>Calças</h4>
-                    </StyledIcons>
-                    <StyledIcons>
-                        <Link to={"/productlisting"}>
-                            <div className="icons-circle">
-                                <img src={bone} alt="Bonés" />
-                            </div>
-                        </Link>
-                        <h4>Bonés</h4>
-                    </StyledIcons>
-                    <StyledIcons>
-                        <Link to={"/productlisting"}>
-                            <div className="icons-circle">
-                                <img src={headphones} alt="headphones" />
-                            </div>
-                        </Link>
-                        <h4>Headphones</h4>
-                    </StyledIcons>
-                    <StyledIcons>
-                        <Link to={"/productlisting"}>
-                            <div className="icons-circle">
-                                <img src={tenis} alt="Tênis" />
-                            </div>
-                        </Link>
-                        <h4>Tênis</h4>
-                    </StyledIcons>
-                </StyledIconsContainer>
-            </Section>
-            <Section
-                title={"Produtos em alta"}
-                titleAlign="space-between"
-                link={"/productlisting"}
-            >
-                <h1>teste</h1>
-            </Section>
-            <Section>
-                <StyledOferta>
-                    <div className="airJordan">
-                        <img src={airJordan} alt="Tênis AirJordan" />
-                    </div>
-                    <div className="informacao-oferta">
-                        <h4>Oferta Especial</h4>
-                        <p className="titulo-oferta">Air Jordan titulo de colecionador</p>
-                        <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum numquam suscipit incidunt architecto commodi quae sapiente aliquid eaque, quo soluta. Omnis minima fugit corrupti pariatur possimus quibusdam voluptatem ex earum!
-                        </p>
-                        <Link to={"/productview"}>
-                        <StyledButtonPrimary> Ver oferta</StyledButtonPrimary>
-                        </Link>
-                    </div>
-                </StyledOferta>
-            </Section>
-        </>
-    );
+  return (
+    <>
+      <Section title={"Coleções em destaque"} color={"#F5F5F5"}>
+        <StyledImgColecao>
+          <div>
+            <img src={collection1} alt="" />
+            <Link to={"/productlisting"}>
+              <StyledButtonSecondary>Comprar</StyledButtonSecondary>
+            </Link>
+          </div>
+          <div>
+            <img src={collection2} alt="" />
+            <Link to={"/productlisting"}>
+              <StyledButtonSecondary>Comprar</StyledButtonSecondary>
+            </Link>
+          </div>
+          <div>
+            <img src={collection3} alt="" />
+            <Link to={"/productlisting"}>
+              <StyledButtonSecondary>Comprar</StyledButtonSecondary>
+            </Link>
+          </div>
+        </StyledImgColecao>
+      </Section>
+      <Section
+        title={"Coleções em destaque"}
+        titleAlign="center"
+        color={"#F5F5F5"}
+      >
+        <StyledIconsContainer>
+          <StyledIcons>
+            <Link to={"/productlisting"}>
+              <div className="icons-circle">
+                <img src={camisetas} alt="camisetas" />
+              </div>
+            </Link>
+            <h4>Camisetas</h4>
+          </StyledIcons>
+          <StyledIcons>
+            <Link to={"/productlisting"}>
+              <div className="icons-circle">
+                <img src={calcas} alt="calças" />
+              </div>
+            </Link>
+            <h4>Calças</h4>
+          </StyledIcons>
+          <StyledIcons>
+            <Link to={"/productlisting"}>
+              <div className="icons-circle">
+                <img src={bone} alt="Bonés" />
+              </div>
+            </Link>
+            <h4>Bonés</h4>
+          </StyledIcons>
+          <StyledIcons>
+            <Link to={"/productlisting"}>
+              <div className="icons-circle">
+                <img src={headphones} alt="headphones" />
+              </div>
+            </Link>
+            <h4>Headphones</h4>
+          </StyledIcons>
+          <StyledIcons>
+            <Link to={"/productlisting"}>
+              <div className="icons-circle">
+                <img src={tenis} alt="Tênis" />
+              </div>
+            </Link>
+            <h4>Tênis</h4>
+          </StyledIcons>
+        </StyledIconsContainer>
+      </Section>
+      <Section
+        title={"Produtos em alta"}
+        titleAlign="space-between"
+        link={"/productlisting"}
+        color={"#F5F5F5"}
+      >
+        <ProductCard image={tenisv8} category={'Tênis'} name={'Tênis v8'} price={100} />
+        <ProductCard image={tenisv8} category={'Teste'} name={'Tênis v8 teste 2'} price={200} priceDiscount={100} />
+      </Section>
+      <Section>
+        <StyledOferta>
+          <div className="airJordan">
+            <img src={airJordan} alt="Tênis Air Jordan" />
+          </div>
+          <div className="informacao-oferta">
+            <h4>Oferta Especial</h4>
+            <p className="titulo-oferta">Air Jordan edição de colecionador</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis,
+              nihil consectetur dolore tenetur similique alias enim pariatur
+              modi cupiditate necessitatibus sequi quaerat corrupti repudiandae
+              adipisci molestiae eaque ipsum repellendus aliquam!
+            </p>
+            <Link to={"/productview"}>
+              <StyledButtonPrimary> Ver oferta </StyledButtonPrimary>
+            </Link>
+          </div>
+        </StyledOferta>
+      </Section>
+    </>
+  );
 }
 
 export default HomePage;
